12799ac817b5de56efe4e36360c581f1f3657924
Kurt Su <phononobserver@gmail.com>
2023-06-08 22:42:29 +0800

    figure_type_update-Y230608
    - Add pair plot
    - Add interaction plot for DOE

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import numpy as np
 # import io
 import plotly.express as px
 # from plotly.subplots import make_subplots
-# import plotly.graph_objects as go
+import plotly.graph_objects as go
 
 @st.cache_data
 def convert_df(df):
@@ -50,12 +50,13 @@ with fig_col1:
     # st.markdown("#### **Choose figure type**")
     fig_type = st.selectbox(
         "### Choose figure type", 
-        ["box", "violin", "scatter", "bubble", "bubble animate", "histogram"],
+        ["box", "violin", "histogram", "pair plot", "interaction(DOE)", "scatter", "bubble", "bubble animate", ],
     )
     
 with fig_col2:
-    category = st.selectbox(
-        "### Choose category", x_list)
+    if uploaded_csv is not None:
+        category = st.selectbox(
+            "### Choose category", x_list)
     
 size_col1, size_col2 = st.columns(2)
 with size_col1:
@@ -70,6 +71,17 @@ st.markdown("----------------")
 if fig_type == "histogram":
     # st.markdown("##### Choose ")
     bins = st.number_input('Choose bins', min_value=5, value=10, max_value=100, step=5) 
+
+elif fig_type == "pair plot":
+    focus_factor = st.multiselect(
+        "### Choose focus factor", df_raw.columns,
+    )
+
+elif fig_type == "interaction(DOE)":
+    focus_factor = st.multiselect(
+        "### Choose focus factor", x_list,
+    )
+
 
 
 if st.button('Plot'):
@@ -98,17 +110,75 @@ if st.button('Plot'):
                     # range_x=x_range, range_y=y_range, template=template,       
                     width=fig_width, height=fig_height
                     )
+    
+    elif fig_type == "pair plot":
+        fig = px.scatter_matrix(df_plot, dimensions=focus_factor, color=category, 
+                             width=fig_width, height=fig_height)
+        fig.update_traces(diagonal_visible=False, showupperhalf=False)
+
+
+    elif fig_type == "interaction(DOE)":
+        iter_list =  list(itertools.combinations(focus_factor, 2))
+
+        # j=1
+                
+        for inter_factor in iter_list:
+            inter_factor
+            factor_1 = inter_factor[0]
+            factor_2 = inter_factor[1]
+            # print(list(i).append(y))
+            # print(factor_1)
+            # print(factor_2)
+            select_list = list(inter_factor)
+            select_list.append(y_var)
+            # print(select_list)
+
+
+
+            df_interact_plot = df_plot[select_list]
+            df_interact_group = df_interact_plot.groupby(list(inter_factor), as_index=False).mean()
+
+
+            fig_scatter = px.scatter(df_interact_plot, x=factor_1, y=y_var, color=factor_2, width=fig_width, height=fig_height)
+            fig_line = px.line(df_interact_group, x=factor_1, y=y_var, color=factor_2, width=fig_width, height=fig_height, markers=True)
+            fig = go.Figure(data=fig_scatter.data + fig_line.data)
+            fig.update_layout(
+                autosize=False,
+                width=fig_width,
+                height=fig_height,
+                xaxis_title=factor_1,
+                yaxis_title=y_var,
+                title=factor_2,
+                legend_title=factor_2,
+                legend_orientation="h"
+                # margin=dict(
+                #     l=50,
+                #     r=50,
+                #     b=100,
+                #     t=100,
+                #     pad=4
+                # ),
+                # paper_bgcolor="LightSteelBlue",
+            )
+            st.plotly_chart(fig, use_container_width=True)
+
+
+
+    if fig_type != "pair plot" or fig_type != "interaction(DOE)":
   
-    fig.update_layout(
-        xaxis_title=x_var,
-        yaxis_title=y_var,
-        # legend_title="Legend Title",
-        font=dict(
-            family="Courier New, monospace",
-            size=18
-            # color="RebeccaPurple"
-        ),
-        yaxis = dict(tickfont = dict(size=25)),
-        xaxis = dict(tickfont = dict(size=15))
-    )
-    st.plotly_chart(fig, use_container_width=True)
+        fig.update_layout(
+            xaxis_title=x_var,
+            yaxis_title=y_var,
+            # legend_title="Legend Title",
+            font=dict(
+                family="Courier New, monospace",
+                size=18
+                # color="RebeccaPurple"
+            ),
+            yaxis = dict(tickfont = dict(size=25)),
+            xaxis = dict(tickfont = dict(size=15))
+        )
+
+    if fig_type != "interaction(DOE)":
+
+        st.plotly_chart(fig, use_container_width=True)
